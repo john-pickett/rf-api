a50c3691c07e12df3feb0fda51c874f99f9595b5
John P <john.pickett@gmail.com>
2018-05-03 06:37:16 -0500

    enable cors

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.listen(port, () => {
     console.log(`Started up at ${port}`)
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, DELETE, PATCH");
+    next();
+  });
+
 app.get('/', (req, res) => {
     console.log('/ hit');
     res.send(parser)
